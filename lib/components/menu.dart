a284cf3c663cec0696daeb5b752d5af03bc1b520
IsaiDiaz <jsoza057@gmail.com>
2024-02-04 17:48:07 -0400

    minor corrections

--- lib/components/menu.dart
+++ lib/components/menu.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wireless_time_guardian_flutter_frontend/bloc/page_bloc.dart';
